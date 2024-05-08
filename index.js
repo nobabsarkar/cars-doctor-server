37d4c2156b754b44182e274dd8c06663d8e89121
nobabsarkar <nobabsarkar2016@gmail.com>
2024-05-08 14:59:46 +0300

    solve error

--- index.js
+++ index.js
@@ -36,23 +36,23 @@ const client = new MongoClient(uri, {
 
 // middlewere
 
-const logger = (req, res, next)=>{
+const logger = (req, res, next) => {
   console.log('log info', req.method, req.url)
   next()
 }
 
 
-const varifyToken = (req, res, next) =>{
+const varifyToken = (req, res, next) => {
   const token = req.cookies?.token;
-    // console.log('token in the middleweare', token)
-  if(!token){
-    return res.status(401).send({message: 'unauthorized access'})
+  // console.log('token in the middleweare', token)
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' })
   }
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     console.log(decoded)
-    if(err){
-      return res.status(401).send({message: 'unauthorize access'})
+    if (err) {
+      return res.status(401).send({ message: 'unauthorize access' })
     }
 
     req.user = decoded;
@@ -75,10 +75,10 @@ async function run() {
 
 
     // auth related api 
-    app.post('/jwt', logger, async(req, res)=>{
+    app.post('/jwt', logger, async (req, res) => {
       const user = req.body;
       // console.log('user for token',user)
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
 
       res.cookie('token', token, {
         httpOnly: true,
@@ -86,23 +86,33 @@ async function run() {
         sameSite: 'none'
       })
 
-      res.send({success: true})
+      res.send({ success: true })
 
     })
 
 
 
-    app.post('/logout', async(req, res)=>{
+    app.post('/logout', async (req, res) => {
       const user = req.body;
-      console.log('logging out',user)
-      res.clearCookie('token', {maxAge: 0}).send({success: true})
+      console.log('logging out', user)
+      res.clearCookie('token', { maxAge: 0 }).send({ success: true })
     })
 
 
 
     // services related api
     app.get('/services', async (req, res) => {
-      const cursor = serviceCollection.find()
+      const filter = req.query;
+      const query = {
+        title: { $regex: filter.search, $options: 'i' }
+
+      }
+      const options = {
+        sort: {
+          price: filter.sort === 'asc' ? 1 : -1
+        }
+      }
+      const cursor = serviceCollection.find(query, options)
       const result = await cursor.toArray()
       res.send(result)
     })
@@ -121,11 +131,11 @@ async function run() {
 
 
     app.get('/bookings', logger, varifyToken, async (req, res) => {
-    console.log(req.user.email)
-    console.log(req.query.email)
+      console.log(req.user.email)
+      console.log(req.query.email)
       // console.log('token owner info', req.user)
-      if(req.user.email !== req.query.email){
-       return res.status(403).send({message: 'forbidden access'})
+      if (req.user.email !== req.query.email) {
+        return res.status(403).send({ message: 'forbidden access' })
       }
       let query = {}
       if (req.query?.email) {
